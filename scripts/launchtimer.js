3ba556d609cd324e5f2082510d91a13e058c8a85
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-06-27 10:01:23 +0100

    added launch timer and delayed it

--- scripts/launchtimer.js
+++ scripts/launchtimer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 1, 2021 17:30:00").getTime();
+var countDownDate = new Date("July 2, 2021 17:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
