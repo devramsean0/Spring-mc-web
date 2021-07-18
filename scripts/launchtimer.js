4ef04a95b8c6faa1f8d28857e610efddefef6c5c
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-07-18 09:02:44 +0100

    uhm

--- scripts/launchtimer.js
+++ scripts/launchtimer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 16, 2021 17:30:00").getTime();
+var countDownDate = new Date("July 30, 2021 17:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,7 +17,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdowntimer").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdowntimer").innerHTML = "Time till hopefull launch " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
